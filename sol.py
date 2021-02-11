cc8a18f232f5e302aa84ad8b784fbc57e8bb6179
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-11 23:54:47 +0530

    read complete

--- sol.py
+++ sol.py
@@ -1,7 +1,20 @@
 def read():
     f = open("inputs/a_example.in", "r")
+    c = 0
+    pmenu = []
     for i in f:
-        print(i)
+        if c == 0:
+            p, t2, t3, t4 = i.rstrip("\\n").split()
+            p = int(p)
+            t2 = int(t2)
+            t3 = int(t3)
+            t4 = int(t4)
+            c += 1
+        else:
+            pmenu.append(i.rstrip("\\n").split())
+            pmenu[c - 1][0] = int(pmenu[c - 1][0])
+            c += 1
+    print(pmenu)
 
 
 read()
